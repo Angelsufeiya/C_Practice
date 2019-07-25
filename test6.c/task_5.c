87288839848f3fbf568731684b3a3664d2f1acf6
武纪东 <Angelsufeiya@hotmail.com>
2019-07-25 23:14:15 +0800

    come on

--- test6.c/task_5.c
+++ test6.c/task_5.c
@@ -5,42 +5,45 @@
 #include<math.h>
 
 //实现一个函数，判断一个数是不是素数。
-//void judgePrimeNum(int n){
-//	int i = 0;
-//	int count = 1;
-//	for (i = 2; i <= sqrt(n); i++){
-//		if (n % i == 0){
-//			count = 0;
-//			break;
-//		}
-//		if (count == 1){
-//			printf("%d\n", n);
-//		}
-//	}
-//}
-//
-//int main(){
-//	int n = 0;
-//	scanf("%d", n);
-//	judgePrimeNum(n);
-//	system("pause");
-//	return 0;
-//}
+void judgePrimeNum(int n){
+	int i = 0;
+	int count = 1;
+	for (i = 2; i <= sqrt(n); i++){
+		if (n % i == 0){
+			count = 0;
+			break;
+		}
+	}
+	if (count == 1){
+		printf("%d是素数\n", n);
+	}
+	else{
+		printf("%d不是素数\n", n);
+	}
+}
 
 int main(){
-	int num, i;
 	int n = 0;
-	for (i = 100; i <= 200; i++){
-		for (num = 2; num <= sqrt(i); num++){
-			if (i%num == 0){
-				n = 1;
-				break;
-			}
-		}
-		if (n == 0)
-			printf("%d\n", i);
-		n = 0;//另n归0是为了重新判断i是否为素数
-	}
+	scanf("%d", &n);
+	judgePrimeNum(n);
 	system("pause");
 	return 0;
 }
+
+//int main(){
+//	int num, i;
+//	int n = 0;
+//	for (i = 100; i <= 200; i++){
+//		for (num = 2; num <= sqrt(i); num++){
+//			if (i%num == 0){
+//				n = 1;
+//				break;
+//			}
+//		}
+//		if (n == 0)
+//			printf("%d\n", i);
+//		n = 0;//另n归0是为了重新判断i是否为素数
+//	}
+//	system("pause");
+//	return 0;
+//}
