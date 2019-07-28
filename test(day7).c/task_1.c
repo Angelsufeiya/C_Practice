5384cf0b55dfaf2756379132bfb4ba190774f83f
武纪东 <Angelsufeiya@hotmail.com>
2019-07-28 23:12:11 +0800

    come on

---
+++ test(day7).c/task_1.c
@@ -0,0 +1,35 @@
+//递归和非递归分别实现求第n个斐波那契数。 
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int fib(int n){
+	if (n <= 2)
+		return 1;
+	else
+		return fib(n - 1) + fib(n - 2);
+}
+
+//int fib(int n){
+//	int a = 1;
+//	int b = 1;
+//	int c = 1;
+//	while (n > 2){
+//		c = a + b;
+//		a = b;
+//		b = c;
+//		n--;
+//	}
+//	return c;
+//}
+
+int main(){
+	int n, ret;
+	scanf("%d", &n);
+	ret = fib(n);
+	printf("%d", ret);
+
+	system("pause");
+	return 0;
+}
