71acdf7a463e3e5cf3b2cc9f2e20c592d68d255a
武纪东 <Angelsufeiya@hotmail.com>
2019-07-30 23:22:22 +0800

    come on

--- test(day7).c/task_1.c
+++ test(day7).c/task_1.c
@@ -4,17 +4,18 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int fib(int n){
-	if (n <= 2)
-		return 1;
-	else
-		return fib(n - 1) + fib(n - 2);
-}
+//int fib(int n){
+//	if (n <= 2)
+//		return 1;
+//	else
+//		return fib(n - 1) + fib(n - 2);
+//}
 
 //int fib(int n){
 //	int a = 1;
 //	int b = 1;
 //	int c = 1;
+//
 //	while (n > 2){
 //		c = a + b;
 //		a = b;
@@ -23,6 +24,20 @@ int fib(int n){
 //	}
 //	return c;
 //}
+
+int fib(int n){
+	int an = 1;
+	int an_1 = 1;
+	int an_2 = 1;
+	int i;
+
+	for(i = 2; i <= n; i++){
+		an = an_1 - an_2;
+		an_2 = an_1;
+		an_1 = an;
+	}
+	return an;
+}
 
 int main(){
 	int n, ret;
