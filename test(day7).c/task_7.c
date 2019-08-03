9e6514750588c846ba182023bfe8489b11959664
武纪东 <Angelsufeiya@hotmail.com>
2019-08-03 23:01:18 +0800

    come on

--- test(day7).c/task_7.c
+++ test(day7).c/task_7.c
@@ -5,6 +5,23 @@
 
 //递归方式实现打印一个整数的每一位 
 
-int main(){
+void printSum(int n){
+	if (n == 0){
+		return;
+	}
 
+	printSum(n / 10);
+	//通过除数的改变可以换算不同进制
+	printf("%d ", n % 10);
+}
+
+int main(){
+	int n;
+
+	scanf("%d", &n);
+	printSum(n);
+	printf("\n");
+
+	system("pause");
+	return 0;
 }
