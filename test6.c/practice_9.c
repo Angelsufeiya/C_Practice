4ff1d2f9e39ac2e8508715caa5f22ec3384efa96
武纪东 <Angelsufeiya@hotmail.com>
2019-08-04 23:18:14 +0800

    comg on

--- test6.c/practice_9.c
+++ test6.c/practice_9.c
@@ -1,12 +1,35 @@
 #define _CRT_SECURE_NO_WARNINGS
 
-#include<stdio.h>
-#include<stdlib.h>
 
 //∫∫≈µÀ˛Œ Ã‚
 
-int main(){
-	int i = 3;
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <string.h>
+#include <iomanip>
+
+using namespace std;
+#define INF 0x3f3f3f3f
+
+void hanno(int n, char a, char b, char c)
+{
+	if (n > 0)
+	{
+		hanno(n - 1, a, c, b);
+		cout << n << ":" << a << "->" << c << endl;
+		hanno(n - 1, b, a, c);
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	//cout<<fixed<<setprecision(2);
 	int n;
+	char a, b, c;
+	cin >> n >> a >> b >> c;
+	hanno(n, a, b, c);
 	return 0;
 }
+
