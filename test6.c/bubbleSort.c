402f39055465b80233a917317c85ec0593f85c24
武纪东 <Angelsufeiya@hotmail.com>
2019-08-05 23:13:00 +0800

    come on

---
+++ test6.c/bubbleSort.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//√∞≈›≈≈–Ú
+
+void bubbleSort(int arr[], int sz){
+	int i, j ,tmp;
+	for (i = 0; i <= sz - 1; i++){	//√∞≈›µƒÃÀ ˝
+		for (j = 0; j < sz - 1 - i; j++){
+			if (arr[j]>arr[j + 1]){
+				tmp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = tmp;
+			}
+		}
+	}
+}
+
+void printArr(int arr[], int sz){
+	int i;
+	for (i = 0; i < sz; i++){
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+}
+
+int main(){
+	int arr[] = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int i = 0;
+
+	printArr(arr, sz);
+	bubbleSort(arr, sz);
+	printArr(arr, sz);
+
+	system("pause");
+	return 0;
+}
--- test6.c/practice_9.c
+++ test6.c/practice_9.c
@@ -3,33 +3,3 @@
 
 //ººÅµËşÎÊÌâ
 
-#include <iostream>
-#include <cstdio>
-#include <cstdlib>
-#include <string.h>
-#include <iomanip>
-
-using namespace std;
-#define INF 0x3f3f3f3f
-
-void hanno(int n, char a, char b, char c)
-{
-	if (n > 0)
-	{
-		hanno(n - 1, a, c, b);
-		cout << n << ":" << a << "->" << c << endl;
-		hanno(n - 1, b, a, c);
-	}
-}
-
-int main()
-{
-	ios::sync_with_stdio(0);
-	//cout<<fixed<<setprecision(2);
-	int n;
-	char a, b, c;
-	cin >> n >> a >> b >> c;
-	hanno(n, a, b, c);
-	return 0;
-}
-
