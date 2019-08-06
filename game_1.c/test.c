548c7a69e082de81dab2dfa218dfacb207304db2
武纪东 <Angelsufeiya@hotmail.com>
2019-08-06 23:06:44 +0800

    come on

---
+++ game_1.c/test.c
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void menu(){
+	printf("*****************************\n");
+	printf("*******    1. play    *******\n");
+	printf("*******    2. exit    *******\n");
+	printf("*****************************\n");
+}
+
+void game(){
+	printf("hehe\n");
+}
+
+int main(){
+	int input;
+
+	do{
+		menu();
+		printf("请选择：");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			game();
+		case 0:
+			printf("游戏已关闭！");
+			break;
+		default:
+			printf("选择错误，请重新选择");
+			break;
+		}
+	} while (input);
+
+	system("pause");
+	return 0;
+}
