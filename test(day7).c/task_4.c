ee47e06f417b491bc3376a62cb619a5fb4de1f40
武纪东 <Angelsufeiya@hotmail.com>
2019-08-20 19:03:15 +0800

    come on

--- test(day7).c/task_4.c
+++ test(day7).c/task_4.c
@@ -2,17 +2,38 @@
 
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
 //编写一个函数 reverse_string(char * string)（递归实现） 
 //实现：将参数字符串中的字符反向排列。
 //要求：不能使用C函数库中的字符串操作函数。
 
-reverse_string(){
+//void reverse_string(char * start, char * end){
+//	char tmp = *start;
+//	*start = *end;
+//	*end = tmp;
+//	if (start + 1 < end - 1){
+//		reverse_string(start + 1, end - 1);
+//	}
+//}
 
+void reverse_string(char * str){
+	int end = strlen(str) - 1;
+	char tmp = str[0];
+	if (*str){
+		str[0] = str[end];
+		str[end] = '\0';
+		reverse_string(str + 1);
+		str[end] = tmp;
+	}
 }
 
 int main(){
-	int arr = "abcdef";
+	char str[10] = "bitekeji";
+
+	//reverse_string(str, str + strlen(str) - 1);
+	reverse_string(str);
+	printf("%s\n", str);
 
 	system("pause");
 	return 0;
