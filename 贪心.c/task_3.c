99ebf7aa828e27f412a7c159a98eaa121ca4a2e3
武纪东 <Angelsufeiya@hotmail.com>
2019-09-23 10:32:06 +0800

    come on

--- 贪心.c/task_3.c
+++ 贪心.c/task_3.c
@@ -1,16 +1,31 @@
-//
+//现在有n堆石子，每堆石子的个数不同，现在将n堆石子合并成一堆石子，
+//每次合并两堆石子，合并过程中会计下石子个数，请问，石子数最低和最大分别是多少？
 
 #define _CRT_SECURE_NO_WARNINGS
 
 #include<stdio.h>
 #include<stdlib.h>
 
-void bubble(int data[], int n){	//冒泡排序(代码最简易的排序）
+void bubble1(int data[], int n){	//冒泡排序(由小到大排序）
 	int i, j, tmp;
 
 	for (i = 0; i < n - 1; i++){
-		for (j = 0; j < n - 1 - i; j++){
+		for (j = i; j < n - 1; j++){
 			if (data[j] > data[j + 1]){
+				tmp = data[j];
+				data[j] = data[j + 1];
+				data[j + 1] = tmp;
+			}
+		}
+	}
+}
+
+void bubble2(int data[], int n){	//冒泡排序(由大到小排序）
+	int i, j, tmp;
+
+	for (i = 0; i < n - 1; i++){
+		for (j = 0; j < n - 1; j++){
+			if (data[j] < data[j + 1]){
 				tmp = data[j];
 				data[j] = data[j + 1];
 				data[j + 1] = tmp;
@@ -24,15 +39,25 @@ int main(){
 	int num[20] = { 0 };
 	int sum = 0;
 
-	printf("请输入岸边人的数量和船的承重：");
+	printf("请输入有多少堆石子：");
 	scanf("%d", &n);
-	printf("请输入每个人的体重：");
-	for (i = 0; i < num; i++){
+	printf("请输入每堆石子中的个数：");
+	for (i = 0; i < n; i++){
 		scanf("%d", num + i);
 	}
-	bubble(num, i);	//将人的体重由小到大排序
+	for (i = 0; i < n - 1; i++){
+		bubble1(num + i, n - i);
+		num[i + 1] = num[i] + num[i + 1];
+		sum += num[i + 1];
+	}
+	printf("石子数最小为 %d\n", sum);
 
-
+	//for (i = 0; i < n - 1; i++){
+	//	bubble2(num + i, n - i);
+	//	sum += num[i] + num[i + 1];
+	//	num[i + 1] = sum;
+	//}
+	//printf("石子数最大为 %d\n", sum);
 
 	system("pause");
 	return 0;
