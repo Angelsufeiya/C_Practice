32104dc89cdd7eee7bded46f437748a3cf4492a7
武纪东 <Angelsufeiya@hotmail.com>
2019-10-03 23:17:26 +0800

    come on

---
+++ test(day14).c/task_02.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+#include<string.h>
+
+int find_round(char * src, char * find){
+	char tmp[256] = { 0 };
+	strcpy(tmp, src);
+	strcat(tmp, src);
+	return strstr(tmp, find) ? 1 : 0;//‘⁄tmp¿Ô√Ê’“find
+}
+
+int main(){
+	printf("%d\n", find_round("ABCDE", "BCDEA"));
+
+	return 0;
+}
