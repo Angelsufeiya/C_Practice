274e48d8f1291c001b52e609cf0ee751c128376d
武纪东 <Angelsufeiya@hotmail.com>
2019-10-08 22:44:39 +0800

    come on

--- test.c/test.c
+++ test.c/test.c
@@ -65,3 +65,21 @@ int main(){
 }
 #endif
 
+
+int check_sys()
+{
+	int i = 1;
+	return (*(char *)&i);
+}
+int main() {
+	int ret = check_sys();
+	if (ret == 1)
+	{
+		printf("Ð¡¶Ë\n");
+	}
+	else
+	{
+		printf("´ó¶Ë\n");
+	}
+	return 0;
+}
