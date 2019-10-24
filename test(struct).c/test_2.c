37097455c5183223955b9c9081794b70512ddcc0
武纪东 <Angelsufeiya@hotmail.com>
2019-10-24 22:50:22 +0800

    come on

--- test(struct).c/test_2.c
+++ test(struct).c/test_2.c
@@ -64,18 +64,35 @@
 //}
 
 //判断当前计算机的大小端存储
-int sys_check(){
-	int a = 1;
-	return *((char*)&a);
-}
+//int sys_check(){
+//	int a = 1;
+//	return *((char*)&a);
+//}
+//
+//int main(){
+//	int n = sys_check();
+//	if (n == 1){
+//		printf("小端\n");//(地位存储低地址)
+//	}
+//	else{
+//		printf("大端\n");
+//	}
+//	return 0;
+//}
 
-int main(){
-	int n = sys_check();
-	if (n == 1){
-		printf("小端\n");//(地位存储低地址)
+
+int main()
+{
+	char arr[51] = { 0 };
+	const char* lp = "|/-\\";
+	for (int i = 0; i < 50; i++)
+	{
+		arr[i] = '#';
+		//   /r 
+		printf("[%-50s][%d%%][%c]\r", arr, i + 1, lp[i % 4]);
+		fflush(stdout);
+		usleep(100000);
 	}
-	else{
-		printf("大端\n");
-	}
+	printf("\n");
 	return 0;
 }
