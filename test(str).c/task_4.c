77caf23f62238bc8dcd9f8e95fdbd7190b7a6f81
武纪东 <Angelsufeiya@hotmail.com>
2019-10-29 22:31:44 +0800

    come on

---
+++ test(str).c/task_4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+//1.模拟实现strncpy 
+//拷贝num个字符从源字符串到目标空间。
+//如果源字符串的长度小于num，则拷贝完源字符串之后，在目标的后边追加0，直到num个。
+
+
+//2.模拟实现strncat
+//Appends the first num characters of source to destination, plus a terminating null-character.
+//If the length of the C string in source is less than num,
+//only the content up to the terminating null - character is copied.
+
+
+//3.模拟实现strncmp
+//比较到出现另个字符不一样或者一个字符串结束或者num个字符全部比较完。
