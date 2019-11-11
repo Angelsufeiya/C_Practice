670e6faf6d3af5f5cdebf31bbd3139e318827e6e
武纪东 <Angelsufeiya@hotmail.com>
2019-11-11 22:47:18 +0800

    come on

--- test(6.1)/test.c
+++ test(6.1)/test.c
@@ -78,12 +78,32 @@
 //	return 0;
 //}
 
-void test() {
-	int *p = (int *)malloc(INT_MAX / 4);
-	*p = 20;//如果p的值是NULL，就会有问题
-	printf("%d", *p);
-	free(p);
-}
+//void test() {
+//	int *p = (int *)malloc(INT_MAX / 4);
+//	// 需要if（NULL！=p）判断，若开辟失败，返回空指针
+//	*p = 20;//如果p的值是NULL，就会有问题
+//	printf("%d", *p);
+//	free(p);
+//}
+
+//void test() {
+//	int i = 0;
+//	int *p = (int *)malloc(10 * sizeof(int));
+//	if (NULL == p)
+//	{
+//		exit(EXIT_FAILURE);//退出函数
+//	}
+//	for (i = 0; i <= 10; i++)
+//	{
+//		*(p + i) = i;//当i是10的时候越界访问
+//	}
+//		free(p);
+//}
+//break 用于退出单层循环
+//continue 用于结束本次循环
+//return 用于函数的退出
+//上面三个是关键字
+//exit这是个函数
 
 int main(){
 	test();
