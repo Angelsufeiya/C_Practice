2e6a65f4097dedc9e2e6cfd9bf8216aa45cd8a06
武纪东 <Angelsufeiya@hotmail.com>
2019-12-03 20:01:07 +0800

    come on

--- test(文件操作)/test.c
+++ test(文件操作)/test.c
@@ -2,6 +2,27 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
+
+//int main()
+//{
+//	char filename[20], ch;
+//	FILE *fp;
+//	printf("Enter a filename: ");
+//	scanf("%s", filename);
+//	getchar();
+//	printf("Enter some characters to output to file: ");
+//	if ((fp = fopen(filename, "w")) == NULL)
+//		printf("fail to open! \n");
+//	else
+//	{
+//		while ((ch = getchar()) != '\101')
+//			fputc(ch, fp);
+//	}
+//	fclose(fp);
+//
+//	return 0;
+//}
 
 //int main() {
 //	int a = 10000;
@@ -188,28 +209,28 @@
 //}
 
 
-enum { 
-	SIZE = 5 
-};
-
-int main(void)
-{
-	double a[SIZE] = { 1.0, 2.0, 3.0, 4.0, 5.0 };
-	double b = 0.0;
-	size_t ret_code = 0;
-	FILE *fp = fopen("test.bin", "wb"); // 必须用二进制模式 
-	fwrite(a, sizeof(*a), SIZE, fp); // 写 double 的数组 
-	fclose(fp);
-	fp = fopen("test.bin", "rb");
-	// 读 double 的数组
-	while ((ret_code = fread(&b, sizeof(double), 1, fp)) >= 1) {
-		printf("%lf\n", b);
-	}
-	if (feof(fp))
-		printf("Error reading test.bin: unexpected end of file\n");
-	else if (ferror(fp)) {
-		perror("Error reading test.bin");
-	}
-	fclose(fp);
-	fp = NULL;
-}
+//enum { 
+//	SIZE = 5 
+//};
+//
+//int main(void)
+//{
+//	double a[SIZE] = { 1.0, 2.0, 3.0, 4.0, 5.0 };
+//	double b = 0.0;
+//	size_t ret_code = 0;
+//	FILE *fp = fopen("test.bin", "wb"); // 必须用二进制模式 
+//	fwrite(a, sizeof(*a), SIZE, fp); // 写 double 的数组 
+//	fclose(fp);
+//	fp = fopen("test.bin", "rb");
+//	// 读 double 的数组
+//	while ((ret_code = fread(&b, sizeof(double), 1, fp)) >= 1) {
+//		printf("%lf\n", b);
+//	}
+//	if (feof(fp))
+//		printf("Error reading test.bin: unexpected end of file\n");
+//	else if (ferror(fp)) {
+//		perror("Error reading test.bin");
+//	}
+//	fclose(fp);
+//	fp = NULL;
+//}
