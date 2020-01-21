e1db2e6deaf582276d02cdfe03590c4b8163ba7f
武纪东 <Angelsufeiya@hotmail.com>
2020-01-21 21:42:19 +0800

    coem on

--- 每日一题/test.cpp
+++ 每日一题/test.cpp
@@ -1,3 +1,12 @@
 //标题:替换空格
-//请实现一个函数，将一个字符串中的每个空格替换成“ % 20”。
-//例如，当字符串为We Are Happy.则经过替换之 后的字符串为We%20Are % 20Happy。
+//请实现一个函数，将一个字符串中的每个空格替换成“%20”。
+//例如，当字符串为We Are Happy.则经过替换之 后的字符串为We%20Are%20Happy。
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main(){
+
+	return 0;
+}
